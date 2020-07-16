6a60963ef12e3d942a1b31a6b523d00ace730fc6
unknown <aodong.zheng@seetatech.com>
2020-07-16 15:17:53 +0800

    feat: update tip

--- app.js
+++ app.js
@@ -22,48 +22,54 @@ async function main () {
     */
     console.log('1- 正在检查全局配置信息...'.bold)
     const SELECT_CONFIG = (await helper(config)).value // 所选部署项目的配置信息
-    console.log(`您选择了部署 ${ SELECT_CONFIG.name }`.info)
-
     const {
-      deployDir
+      name,
+      openCompress,
+      deployDir,
+      targetFile,
+      targetDir,
+      releaseDir,
+      docker_file,
+      docker_compose
     } = SELECT_CONFIG
-    // get deploy dir name
-    // console.log(SELECT_CONFIG.targetDir.split('/')[SELECT_CONFIG.targetDir.split('/').length - 1])
-
+    console.log(`您选择了部署 ${ name }`.info)
     /* 本地压缩 处理流程 */
-    console.log('2- 文件本地压缩处理...')
-    const localFile =  __dirname + '/' + SELECT_CONFIG.targetFile // 待上传本地文件
-    SELECT_CONFIG.openCompress ? 
-      await compress(getAbsolutePath(SELECT_CONFIG.targetDir), localFile) :
+    console.log('2- 文件本地压缩处理...'.bold)
+    const localFile =  __dirname + '/' + targetFile // 待上传本地文件
+    openCompress ? 
+      await compress(getAbsolutePath(targetDir), localFile) :
       console.log('未开启本地压缩，已跳过'.warn) // 处理是否压缩
     /* ssh 连接 */
+    console.log('3- 执行SSH连接'.bold)
     await connectServe(SELECT_CONFIG.ssh)
     /* 
       物理 部署流程
       备份检查 --> 文件上传 --> 解压、修改、删除文件
     */
     await backup(ssh, SELECT_CONFIG) // 根据配置决定是否备份
-    console.log('4- 开始文件上传')
-    await uploadFile(ssh, localFile, SELECT_CONFIG.deployDir + SELECT_CONFIG.targetFile) // upload target file
-    await runCommand(ssh, 'unzip ' + SELECT_CONFIG.targetFile, SELECT_CONFIG.deployDir) // unzip
-    await runCommand(ssh, 'mv dist ' + SELECT_CONFIG.releaseDir, SELECT_CONFIG.deployDir) // 修改文件名称
-    await runCommand(ssh, 'rm -f ' + SELECT_CONFIG.targetFile, SELECT_CONFIG.deployDir) // clear zip file
+    console.log('4- 开始文件上传'.bold)
+    await uploadFile(ssh, localFile, deployDir + targetFile) // upload target file
+    await runCommand(ssh, 'unzip ' + targetFile, deployDir) // unzip
+    await runCommand(ssh, 'mv dist ' + releaseDir, deployDir) // 修改文件名称
+    await runCommand(ssh, 'rm -f ' + targetFile, deployDir) // clear zip file
     /* 
       docker 部署流程
       docker env check --> upload Dockerfile --> build image 
       upload docker-compose --> run docker-compose --> show container
     */
     await dockerInspect(ssh)
-    const dockerFilePath = SELECT_CONFIG.deployDir + SELECT_CONFIG.releaseDir
-    await uploadFile(ssh, getAbsolutePath(SELECT_CONFIG.docker_file), dockerFilePath + '/Dockerfile') // upload Dockerfile
-    console.log('5- 开始构建docker镜像...')
-    await runCommand(ssh, 'docker build -t test/web:dev .', dockerFilePath)
-    await uploadFile(ssh, getAbsolutePath(SELECT_CONFIG.docker_compose), dockerFilePath + '/docker-compose.yml') // upload Dockerfile
+    const dockerFilePath = deployDir + releaseDir
+    await uploadFile(ssh, getAbsolutePath(docker_file), dockerFilePath + '/Dockerfile') // upload Dockerfile
+    console.log('5- 开始构建docker镜像...'.bold)
+    //TODO 构建镜像需要可配
+    await runCommand(ssh, 'docker build -t spa/web:spa .', dockerFilePath)
+    await uploadFile(ssh, getAbsolutePath(docker_compose), dockerFilePath + '/docker-compose.yml') // upload Dockerfile
     console.log('6- 正在运行docker-compose...请耐心等待')
     await runCommand(ssh, 'docker-compose up -d', dockerFilePath)
     // 显示当前运行中容器
-    console.log('当前运行中的容器：')
+    console.log('7- 当前运行中的容器...'.bold)
     await runCommand(ssh, 'docker ps', dockerFilePath)
+    console.log('🎉恭喜！部署成功🎉'.success)
   } catch (err) {
     console.log('部署过程出现错误！'.error, err)
   } finally {
--- src/inspect/docker_inspect.js
+++ src/inspect/docker_inspect.js
@@ -2,7 +2,9 @@ const runCommand = require ('../ssh/handleCommand')
 
 async function dockerInspect (ssh) {
   await runCommand(ssh, `docker -v`, '/')
+  console.log('docker 环境正常'.success)
   await runCommand(ssh, `docker-compose -v`, '/')
+  console.log('docker-compose 环境正常'.success)
 }
 
 module.exports = dockerInspect
